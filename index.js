057a61d6b1cfd37db5191b38d2b327d2b0389522
aminulrony1999 <aminulrony1999@gmail.com>
2024-01-08 07:59:10 +0530

    news data included

--- index.js
+++ index.js
@@ -3,12 +3,16 @@ const cors = require('cors');
 const app = express();
 const port = 5000;
 const categories = require('./Data/categories.json');
+const news = require('./Data/news.json');
 app.use(cors());
 app.get("/", (req, res) => {
   res.send("Dragon news running!");
 });
 app.get("/categories",(req,res) => {
     res.send(categories);
+})
+app.get("/news",(req,res) =>{
+  res.send(news);
 })
 app.listen(port, () => {
   console.log(`Dragon news is running on port ${port}`);
